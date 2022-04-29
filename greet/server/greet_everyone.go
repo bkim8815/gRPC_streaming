bc73667602a6d1586696e9c5d5f9e67f092a537d
Brian Kim <bkim8815@gmail.com>
2022-04-29 10:08:59 -0400

    bi-directional streaming done

---
+++ greet/server/greet_everyone.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"io"
+	"log"
+
+	pb "github.com/bkim8815/gRPC_streaming/greet/proto"
+)
+
+func (s *Server) GreetEveryone(stream pb.GreetService_GreetEveryoneServer) error {
+	log.Println("GreetEveryone was incoked")
+
+	for {
+		req, err := stream.Recv()
+
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			log.Fatalf("Error while reading clinet streamd %v\n", err)
+		}
+
+		res := "Hello " + req.FirstName + "!"
+		err = stream.Send(&pb.GreetResponse{
+			Result: res,
+		})
+
+		if err != nil {
+			log.Fatalf("Error while sending clinet streamd %v\n", err)
+
+		}
+	}
+
+}
